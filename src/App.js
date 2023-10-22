4bca00aa046d66812d4c3d2724cff57e9d8738d9
Pedro Lorenzo Rosario <63958751+PALR-1216@users.noreply.github.com>
2023-10-22 02:18:23 -0400

    sds

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-
 import './App.css';
 import Header from './Header';
-
+import Test from './Test';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//learn to use ionic v6
 
 function App() {
-return(
-  <>
-  <Header/>
-  </>
-)
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Header />} />
+        <Route path="/test" element={<Test />} />
+        {/* Add more routes here as needed */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,58 +1,62 @@
 import React from "react";
 import carWash from './carWash.jpg'
-import CarWashVideo from './carwashVideo.mp4'
+import {Link} from 'react-router-dom'
+// import CarWashVideo from './carwashVideo.mp4'
 
 
-// function Header() {
+function Header() {
 
-//     return (
-//         <div className="h-screen bg-cover bg-center bg-gradient-to-r from-indigo-800 to-indigo-500 relative" style={{
-//             backgroundImage: `url(${carWash})`
+    return (
+        <div className="h-screen bg-cover bg-center bg-gradient-to-r from-indigo-800 to-indigo-500 relative" style={{
+            backgroundImage: `url(${carWash})`
 
-//         }}>
-//              <div className="absolute inset-0 bg-black opacity-60"></div>
-//             <div className='absolute inset-0 flex items-center justify-center text-white'>
-//                 <div className="text-center">
-//                     <h1 className="text-4xl font-bold">Car washing Service</h1>
-//                     <p className="text-lg mt-2">hero descriptiom</p>
-//                     <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Button</button>
+        }}>
+             <div className="absolute inset-0 bg-black opacity-60"></div>
+            <div className='absolute inset-0 flex items-center justify-center text-white'>
+                <div className="text-center">
+                    <h1 className="text-4xl font-bold">Car washing Service</h1>
+                    <p className="pb-4 text-lg mt-2">hero descriptiom</p>
+                    <Link to="/test"> {/* Use Link to navigate to the "new-page" route */}
+                        <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
+                     </Link>
 
-//                 </div>
-//             </div>
+
+                </div>
+            </div>
            
-//         </div>
+        </div>
         
-//       );
+      );
 
 
-// }
+}
 
 // export default Header;
 
 
 
 
-function Header() {
+// function Header() {
 
-    return (
-        <div className="h-screen relative">
-      <video autoPlay muted loop className="w-full h-full object-cover">
-        <source src={CarWashVideo} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-      <div className="absolute inset-0 bg-black opacity-60"></div>
-      <div className="absolute inset-0 flex items-center justify-center text-white">
-        <div className="text-center">
-          <h1 className="text-4xl font-bold">Sebastean car Wash</h1>
-          <p className="text-lg mt-2">Hero Description</p>
-        </div>
-      </div>
-    </div>
+//     return (
+//         <div className="h-screen relative">
+//       <video autoPlay muted loop className="w-full h-full object-cover">
+//         <source src={CarWashVideo} type="video/mp4" />
+//         Your browser does not support the video tag.
+//       </video>
+//       <div className="absolute inset-0 bg-black opacity-60"></div>
+//       <div className="absolute inset-0 flex items-center justify-center text-white">
+//         <div className="text-center">
+//           <h1 className="text-4xl font-bold">Sebastean car Wash</h1>
+//           <p className="text-lg mt-2">Hero Description</p>
+//         </div>
+//       </div>
+//     </div>
         
-    );
+//     );
 
 
-}
+// }
 
 export default Header;
 
