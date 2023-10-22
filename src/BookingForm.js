12fc2a50b2263452a9ab9e7c7af9d13f510014c8
Pedro Lorenzo Rosario <63958751+PALR-1216@users.noreply.github.com>
2023-10-22 18:03:05 -0400

    ddfd

--- src/BookingForm.js
+++ src/BookingForm.js
@@ -1,58 +1,64 @@
 import React, { useState } from 'react';
-import DatePicker from 'react-datepicker';
-import Select from 'react-select';
-import 'react-datepicker/dist/react-datepicker.css';
-import 'react-datepicker/dist/react-datepicker-cssmodules.css';
-import { format, addDays } from 'date-fns'; // Import date-fns functions
 
 const BookingForm = () => {
-  const [selectedDate, setSelectedDate] = useState(null);
-  const [selectedTime, setSelectedTime] = useState(null);
+  const [date, setDate] = useState(null);
+  const [time, setTime] = useState(null);
 
-  const handleDateChange = (date) => {
-    setSelectedDate(date);
+  const availableDates = ['Mon Oct 09 2023'];
+
+  const availableTimes = {
+    'Mon Oct 09 2023': [
+      'Morning 9:00 am',
+      'Morning 10:00 am',
+      'Morning 11:00 am',
+      'Afternoon 12:00 pm',
+      'Afternoon 2:00 pm',
+    ],
   };
 
-  // Generate a list of times in 12-hour format
-  const times = [
-    '8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM',
-    '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM',
-    '4:00 PM', '5:00 PM', '6:00 PM', '7:00 PM'
-  ];
+  const handleDateChange = (e) => {
+    setDate(e.target.value);
+    // Reset the selected time when the date changes
+    setTime(null);
+  };
+
+  const handleTimeChange = (e) => {
+    setTime(e.target.value);
+  };
 
   return (
-    <div className="container mx-auto mt-8 flex justify-center">
-      <div className="max-w-md mx-auto bg-white rounded-lg shadow-lg p-6 w-full">
-        <h2 className="text-2xl font-semibold mb-4">Book an Appointment</h2>
+    <div className="p-8 bg-gray-100">
+      <h1 className="text-3xl font-bold mb-4">Select Date</h1>
+      <select
+        className="w-full p-2 border border-gray-300 rounded"
+        value={date}
+        onChange={handleDateChange}
+      >
+        <option value="">Select Date</option>
+        {availableDates.map((date) => (
+          <option key={date} value={date}>
+            {date}
+          </option>
+        ))}
+      </select>
 
-        <div className="mb-4 flex justify-center"> {/* Center the date picker */}
-          <label className="block text-gray-600 text-sm font-semibold">Select Date</label>
-          <DatePicker
-            selected={selectedDate}
-            onChange={handleDateChange}
-            minDate={new Date()}
-            filterDate={date => date >= new Date()}
-            className="w-full p-2 border rounded"
-            dateFormat="MM/dd/yyyy" // Date format
-          />
+      {date && (
+        <div>
+          <h2 className="text-2xl font-bold mt-4 mb-2">Available Times</h2>
+          <select
+            className="w-full p-2 border border-gray-300 rounded"
+            value={time}
+            onChange={handleTimeChange}
+          >
+            <option value="">Select Time</option>
+            {availableTimes[date].map((time) => (
+              <option key={time} value={time}>
+                {time}
+              </option>
+            ))}
+          </select>
         </div>
-
-        {selectedDate && (
-          <div className="mb-4">
-            <label className="block text-gray-600 text-sm font-semibold">Select Time</label>
-            <Select
-              options={times.map((time) => ({ value: time, label: time }))}
-              value={selectedTime}
-              onChange={(time) => setSelectedTime(time)}
-              className="w-full p-2 border rounded"
-            />
-          </div>
-        )}
-
-        <button className="bg-blue-500 text-white py-2 px-4 rounded-full">
-          Book Now
-        </button>
-      </div>
+      )}
     </div>
   );
 };
